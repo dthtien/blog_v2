08c38ab2675ff13421ca6b7d15572db5f3fcb8a8
The Tiến <tiendt2311@gmail.com>
2017-05-18 22:57:02 +0700

    fix search model

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,17 +5,17 @@ class Article < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   before_save :set_description
 
-  validates :title, presence: true, length: {minimum: 10, maximum: 90}
+  validates :title, presence: true, length: {minimum: 10, maximum: 120}
   validates :body, presence: true, length: {minimum: 100}
 
   self.per_page = 10
 
-  def self.search
-    
+  searchable do
+    text :title, :body
   end
 
   private
     def set_description
-      self.description = body[0..50]
+      self.description = body[0..120]
     end
 end
