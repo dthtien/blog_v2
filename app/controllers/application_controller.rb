6284961f4f37364f325a32a6c0f7489240eef804
The Tiến <tiendt2311@gmail.com>
2017-05-19 11:39:46 +0700

    Rescue an error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+  rescue_from ActionController::RoutingError with: :not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  protected
+    def not_found
+      redirect_to root_path, alert: "Page not found"
+    end
+
+     def not_authorized
+      redirect_to root_path, alert: "Bạn không có quyền truy cập "
+    end
 end
